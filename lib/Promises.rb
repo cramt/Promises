0a00d5c7e1a1f87c9cad4c884513efe86ecfc569
Alexandra Østermark <alex.cramt@gmail.com>
2020-04-11 00:12:40 +0200

    constructor for promises created and await as well

--- lib/Promises.rb
+++ lib/Promises.rb
@@ -1,12 +1,42 @@
 require "Promises/version"
 
 module Promises
-  class Error < StandardError; end
+  class Error < StandardError
+    def initialize
 
+    end
+  end
 end
 
 class Promise
+  @value
+  @thread
+  @reason
+
+  def initialize(&block)
+    @thread = Thread.new {
+      wait_thread = Thread.new {sleep}
+      block.call(proc {|value|
+        @value = value
+        wait_thread.terminate
+      }, proc {|reason|
+        @reason = reason
+        wait_thread.terminate
+      })
+      wait_thread.join
+    }
+  end
+
   def pending?
     true
+  end
+
+  def await
+    @thread.join
+    if @reason
+      raise @reason
+    else
+      return @value
+    end
   end
 end
--- spec/Promises_spec.rb
+++ spec/Promises_spec.rb
@@ -1,11 +1,19 @@
 RSpec.describe Promises do
-  it "has a version number" do
-    expect(Promises::VERSION).not_to be nil
-  end
+    it "has a version number" do
+        expect(Promises::VERSION).not_to be nil
+    end
 end
 
 RSpec.describe Promise do
-  it "is initially pending" do
-    expect(Promise.new {sleep}).to be_pending
-  end
+    it "is initially pending" do
+        expect(Promise.new { sleep }).to be_pending
+    end
+end
+
+RSpec.describe Promise do
+    it "it raises error on rejects" do
+        expect {
+          Promise.new { |resolve, reject| reject.call "hello" }.await
+        }.to raise_error "hello"
+    end
 end
